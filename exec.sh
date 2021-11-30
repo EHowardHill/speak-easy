3eb63a90a4b6b6d75eb27d954a32eca802de74a2
Ethan Hill <ethan.hill1999@outlook.com>
2021-11-30 12:38:44 -0600

    First example

---
+++ exec.sh
@@ -0,0 +1,11 @@
+pip install -r requirements.txt
+pipwin install pyaudio
+
+echo "- - -"
+echo "Make sure to set the GOOGLE_APPLICATION_CREDENTIALS env variable. Set it to the location of your .json key."
+echo " "
+echo "Also, make sure to install FFMPEG from https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-11-30-12-21/ffmpeg-N-104704-ge22dff43e7-win64-gpl.zip"
+echo " "
+echo "You'll need to put the binaries somewhere within your environment."
+echo "My advice is just to copy the .exe into your Windows folder if you're lazy,"
+echo "that way you won't need to reset your computer."
